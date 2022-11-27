660e745d8b4e73edb26e0bd2835c725e068b2a05
«Nina <ninelle22@yandex.ru>
2022-11-27 12:28:55 +0200

    TASK27

---
+++ TASK27/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+using static System.Console;
+Clear();
+
+int SumofdigitsinNumber (int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /=10;   
+    }
+return sum;
+}
+
+WriteLine("Enter Number: ");
+int number = Convert.ToInt32(ReadLine());
+WriteLine($"Sum of all digits in number = {SumofdigitsinNumber(number)}");
